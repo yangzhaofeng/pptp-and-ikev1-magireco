c366d598c46c9cf611a2781b629bfac17cbeac6c
楊肇峰 <yangzhaofengsteven@gmail.com>
2018-05-16 23:35:30 +0800

    Create docker-debian.bash

---
+++ docker-debian.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ `dpkg -l | grep docker |wc -l` -ne 1 ];then
+apt-get update
+apt-get install \
+	apt-transport-https \
+	ca-certificates \
+	curl \
+	gnupg2 \
+	software-properties-common
+curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | apt-key add -
+add-apt-repository \
+	"deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/debian \
+	$(lsb_release -cs) \
+	stable"
+apt-get update
+apt-get install -y docker-ce
+mkdir /etc/docker
+touch /etc/docker/daemon.json
+echo '{' >> /etc/docker/daemon.json
+echo '	'"registry-mirrors": ["https://docker.mirrors.ustc.edu.cn/"] >> /etc/docker/daemon.json
+echo '}' >> /etc/docker/daemon.json
+systemctl restart docker
+mkdir /srv/docker
+fi
