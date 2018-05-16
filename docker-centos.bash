ac4dfac2f87bc53fe8b7ca280c879546a9115480
楊肇峰 <yangzhaofengsteven@gmail.com>
2018-05-16 23:52:24 +0800

    Create docker-centos.bash

---
+++ docker-centos.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ `yum list installed | grep docker |wc -l` -ne 1 ];then
+yum makecache
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+wget https://mirrors.ustc.edu.cn/docker-ce/linux/centos/docker-ce.repo
+sed -i 's/download.docker.com/mirrors.ustc.edu.cn\/docker-ce/g' docker-ce.repo
+yum-config-manager --add-repo docker-ce.repo
+yum makecache
+yum install -y docker-ce
+mkdir /etc/docker
+touch /etc/docker/daemon.json
+echo '{' >> /etc/docker/daemon.json
+echo '	'"registry-mirrors": ["https://docker.mirrors.ustc.edu.cn/"] >> /etc/docker/daemon.json
+echo '}' >> /etc/docker/daemon.json
+systemctl restart docker
+mkdir /srv/docker
+fi
