ff20db8500a67dba69483044e9820e0994c8d510
楊肇峰 <yangzhaofengsteven@gmail.com>
2018-05-17 00:07:33 +0800

    Update ikev1.bash

--- ikev1.bash
+++ ikev1.bash
@@ -5,8 +5,8 @@ docker run -itd \
 --net=host \
 --privileged=true \
 --name=strongswan-magireco \
--e IPSEC_DNS1=8.8.8.8 \
--e IPSEC_DNS2=8.8.4.4 \
+-e IPSEC_DNS1=192.168.142.254 \
+-e IPSEC_DNS2=101.6.6.6 \
 -e IPSEC_CLIENT_ADDR=10.250.20.0/24 \
 -e IPSEC_PSK=magireco.moe \
 -e XAUTH_USER=qb \
