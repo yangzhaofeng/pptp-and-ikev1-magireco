a9e919a8bb7e405d13bac94178efb1aaa3a47b5d
楊肇峰 <yangzhaofengsteven@gmail.com>
2018-05-26 20:55:49 +0800

    Update ikev1.bash

--- ikev1.bash
+++ ikev1.bash
@@ -8,7 +8,7 @@ docker run -itd \
 -e IPSEC_DNS1=192.168.142.254 \
 -e IPSEC_DNS2=101.6.6.6 \
 -e IPSEC_CLIENT_ADDR=10.250.20.0/24 \
--e IPSEC_PSK=magireco.moe \
--e XAUTH_USER=qb \
--e XAUTH_PASS=magireco \
+-e IPSEC_PSK=$IKEPSK \
+-e XAUTH_USER=$VPN_USER \
+-e XAUTH_PASS=$VPN_PASS \
 yangzhaofengsteven/strongswan-magireco
