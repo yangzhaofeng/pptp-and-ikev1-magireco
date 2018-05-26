59351858b01055ee1f5adb023b6dd5eb4c3ea327
楊肇峰 <yangzhaofengsteven@gmail.com>
2018-05-26 20:54:07 +0800

    Update pptp.bash

--- pptp.bash
+++ pptp.bash
@@ -4,8 +4,8 @@ docker run -itd \
 --restart=always \
 --privileged \
 --net=host \
--e PPP_USER=qb \
--e PPP_PASS=magireco \
+-e PPP_USER=$VPN_USER \
+-e PPP_PASS=$VPN_PASS \
 -e MS_DNS1=192.168.142.254 \
 -e MS_DNS2=101.6.6.6 \
 -e LOCAL_SUBNET=10.250.80 \
